fc5d705f176bdf085963780cb6e3c6e00f2eb6f6
Abhidha Dixit <abhidhadixit0430@gmail.com>
2024-12-07 20:17:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Cards from './components/Cards';
+function App() {
+  return (
+    <div className="App">
+    <h2> MemoryTestor </h2> 
+    <Cards />
+    </div>
+  );
+}
+
+export default App;
